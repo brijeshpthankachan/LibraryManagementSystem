8914f9739c0a767beb7a2d5b4c510e5fc32c81ff
brijeshpthankachan <brijeshpthankachan@gmail.com>
2023-10-04 11:19:27 +0530

    added service registry and npgSQL

---
+++ ApiServices/LMS/LMS.Infrastructure/InfrastructureServicesRegistry.cs
@@ -0,0 +1,22 @@
+﻿using LMS.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LMS.Infrastructure;
+
+public static class InfrastructureServicesRegistry
+{
+    public static void AddInfrastructureRegistryServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        AddPostgres(services, configuration);
+    }
+
+    internal static void AddPostgres(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddEntityFrameworkNpgsql().AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("PostgreSQL")));
+
+    }
+
+}
