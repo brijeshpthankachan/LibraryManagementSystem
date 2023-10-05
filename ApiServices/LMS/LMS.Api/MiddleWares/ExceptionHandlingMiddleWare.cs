6dc321a5f91dfc9b175c266af8592afc6d94bf72
brijeshpthankachan <brijeshpthankachan@gmail.com>
2023-10-05 12:49:17 +0530

    added exception handling middleware

---
+++ ApiServices/LMS/LMS.Api/MiddleWares/ExceptionHandlingMiddleWare.cs
@@ -0,0 +1,73 @@
+﻿using LMS.Domain.Exceptions;
+using System.Text.Json;
+using ApplicationException = LMS.Domain.Exceptions.ApplicationException;
+
+namespace LMS.Api.MiddleWares;
+
+internal sealed class ExceptionHandlingMiddleWare : IMiddleware
+{
+    private readonly ILogger<ExceptionHandlingMiddleWare> _logger;
+
+    public ExceptionHandlingMiddleWare(ILogger<ExceptionHandlingMiddleWare> logger) => _logger = logger;
+
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, e.Message);
+
+            await HandleExceptionAsync(context, e);
+        }
+    }
+
+    private static async Task HandleExceptionAsync(HttpContext httpContext, Exception exception)
+    {
+        var statusCode = GetStatusCode(exception);
+
+        var response = new
+        {
+            title = GetTitle(exception),
+            status = statusCode,
+            detail = exception.Message,
+            errors = GetErrors(exception)
+        };
+
+        httpContext.Response.ContentType = "application/json";
+
+        httpContext.Response.StatusCode = statusCode;
+
+        await httpContext.Response.WriteAsync(JsonSerializer.Serialize(response));
+    }
+
+    private static int GetStatusCode(Exception exception) =>
+        exception switch
+        {
+            //BadRequestException => StatusCodes.Status400BadRequest,
+            //NotFoundException => StatusCodes.Status404NotFound,
+            ValidationException => StatusCodes.Status422UnprocessableEntity,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+    private static string GetTitle(Exception exception) =>
+        exception switch
+        {
+            ApplicationException applicationException => applicationException.Title,
+            _ => "Server Error"
+        };
+
+    private static IReadOnlyDictionary<string, string[]> GetErrors(Exception exception)
+    {
+        IReadOnlyDictionary<string, string[]> errors = null;
+
+        if (exception is ValidationException validationException)
+        {
+            errors = validationException.ErrorsDictionary;
+        }
+
+        return errors;
+    }
+}
