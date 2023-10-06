eed9a6a7b5b735a557d175ab92c84984d342396e
brijeshpthankachan <brijeshpthankachan@gmail.com>
2023-10-06 11:58:44 +0530

    added translation module

--- lms-admin-ui/src/app/app.component.ts
+++ lms-admin-ui/src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { TranslateModule, TranslateService } from '@ngx-translate/core'
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [CommonModule, RouterOutlet],
+	imports: [CommonModule, RouterOutlet, TranslateModule],
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 	title = 'lms-admin-ui'
+
+	constructor(public translate: TranslateService) {
+		translate.addLangs(['en', 'fr'])
+		translate.setDefaultLang('en')
+
+		// This language will be used as a fallback when a translation isn't found in the current language
+		// translateService.setDefaultLang('en')
+		// the lang to use, if the lang isn't available, it will use the current loader to get them
+		// this.translateService.use(this.appConstants.defaultLanguage)
+
+		// const browserLang = translate.getBrowserLang()
+		// translate.use(browserLang.match(/en|fr/) ? browserLang : 'en')
+
+
+	}
 }
--- lms-admin-ui/src/app/app.config.ts
+++ lms-admin-ui/src/app/app.config.ts
@@ -1,9 +1,22 @@
-import { ApplicationConfig } from '@angular/core'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { ApplicationConfig, importProvidersFrom } from '@angular/core'
 import { provideAnimations } from '@angular/platform-browser/animations'
-import { provideRouter } from '@angular/router'
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
 
-import { routes } from './app.routes'
+export const createTranslateLoader = (http: HttpClient) => new TranslateHttpLoader(http, './assets/i18n/', '.json')
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideRouter(routes), provideAnimations()]
+	providers: [
+		provideAnimations(),
+		importProvidersFrom(HttpClientModule), // or provideHttpClient() in Angular v15
+		importProvidersFrom(TranslateModule.forRoot({
+			loader: {
+				provide: TranslateLoader,
+				useFactory: createTranslateLoader,
+				deps: [HttpClient]
+			}
+		})),
+	]
 }
+
