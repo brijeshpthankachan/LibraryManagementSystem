30e0663a032e0f4608ef3c7af5aab720c6e8c476
brijeshpthankachan <brijeshpthankachan@gmail.com>
2023-10-06 10:16:27 +0530

    initialized admin ui project

--- lms-admin-ui/src/app/app.component.spec.ts
+++ lms-admin-ui/src/app/app.component.spec.ts
@@ -1,18 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -20,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lmsUi'`, () => {
+  it(`should have the 'lms-admin-ui' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('lmsUi');
+    expect(app.title).toEqual('lms-admin-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('lmsUi app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('lms-admin-ui app is running!');
   });
 });
